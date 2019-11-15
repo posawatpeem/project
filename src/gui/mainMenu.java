14f96bf1d52defab158e8136fa71578aff32fb96
Posawat Maileung <posawatpeem@gmail.com>
2019-11-16 01:36:44 +0700

    midnight push

--- src/gui/mainMenu.java
+++ src/gui/mainMenu.java
@@ -1,12 +1,21 @@
 package gui;
 
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
 public class mainMenu extends VBox{
 	
 	public mainMenu () {
-		this.setPrefWidth(200);
-		this.setPrefHeight(500);
+		this.setPrefWidth(450);
+		this.setPrefHeight(600);
+		
+		Button play = new Button("Play");
+		Button howTo = new Button("How to play");
+		Button exit = new Button("Exit");
+		play.setPrefWidth(150);
+		play.setPrefHeight(50);
+		play.setStyle(
+			"-fx-text-fill: white; -fx-font-weight: bold; -fx-font-family: \"Arial Narrow\"; -fx-background-color: darkgreen;");
 	}
 
 }
