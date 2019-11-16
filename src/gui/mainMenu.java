edf5e4f3cbfe8e498a9fde4d6a6554597e984775
Posawat Maileung <posawatpeem@gmail.com>
2019-11-16 22:45:58 +0700

    test

--- src/gui/mainMenu.java
+++ src/gui/mainMenu.java
@@ -16,7 +16,7 @@ public class mainMenu extends VBox{
 		play.setPrefHeight(50);
 		play.setStyle(
 			"-fx-text-fill: white; -fx-font-weight: bold; -fx-font-family: \"Arial Narrow\"; -fx-background-color: darkgreen;");
-		play.setPrefHeight(600);
+		play.setPrefHeight(500);
 	}
 
 }
