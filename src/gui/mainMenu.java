5dad0afcabe76d913c23e5eb9f005ad4553e1067
Posawat Maileung <posawatpeem@gmail.com>
2019-11-19 14:39:30 +0700

    push

--- src/gui/mainMenu.java
+++ src/gui/mainMenu.java
@@ -1,10 +1,21 @@
 package gui;
 
+import java.net.URISyntaxException;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class mainMenu extends GridPane{
@@ -12,14 +23,43 @@ public class mainMenu extends GridPane{
 	private Buttons playButt;
 	private Buttons howToButt;
 	private Buttons exitButt;
+	private Image MAIN_BG;
 	
 	public mainMenu () {
+		
 		this.setPrefWidth(400);
 		this.setPrefHeight(600);
+		
+		try {
+			MAIN_BG = new Image(ClassLoader.getSystemResource("bg.jpg").toURI().toString());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		
+		BackgroundImage wallpaper = new BackgroundImage(MAIN_BG,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);;
+		this.setBackground(new Background(wallpaper));
+		
+		Text scenetitle1 = new Text("Foxxy");
+		Text scenetitle2 = new Text("Road.");
+		
+		scenetitle1.setFont(Font.loadFont("Kenney_Future.ttf",96));
+		scenetitle1.setFill(Color.WHITE);
+		scenetitle1.setStrokeWidth(3);
+		scenetitle1.setStroke(Color.LIGHTGRAY);
+		
+		scenetitle2.setFont(Font.loadFont("Kenney_Future.ttf",96));
+		scenetitle2.setFill(Color.WHITE);
+		scenetitle2.setStrokeWidth(3);
+		scenetitle2.setStroke(Color.LIGHTGRAY);
 		
 		this.playButt = new Buttons("PLAY");
 		this.howToButt = new Buttons("HOW TO PLAY");
 		this.exitButt = new Buttons("EXIT");
+		this.initializeMain();
 		
 		
 	}
