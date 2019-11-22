ecc7550c50d98938c629fc1afb8b59a6b16357d5
Posawat Maileung <posawatpeem@gmail.com>
2019-11-22 10:58:06 +0700

    upup

--- src/gui/howToPlay.java
+++ src/gui/howToPlay.java
@@ -1,5 +1,16 @@
 package gui;
 
-public class howToPlay {
+import javafx.scene.layout.VBox;
 
+public class howToPlay extends VBox {
+	
+	public static final int Width = 300 ; 
+	public static final int Height = 500 ; 
+	 
+	public howToPlay() {
+		
+		setPrefWidth(Width);
+		setPrefHeight(Height);
+		
+	}
 }
